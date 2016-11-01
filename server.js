16801cc294eb5958ca3967ab85f1ff4d3cae968a
basslalo <hijodelmetal92@gmail.com>
2016-11-01 16:06:31 -0600

    Server html básico funcionando

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 var http = require ('http');
+//var color = require ('colors');
+//Estableciendo tema de colores 
+/*colors.setTheme({
+    'data' : 'cyan',
+    'info' : 'rainbow',
+    'error' : 'red'
+});
+*/
 
 var server = http.createServer(function(req, res){
     res.writeHead(200,{
-        'Content-Type':'text/plain'
+        'Content-Type':'text/html'
     });
-    res.end('> Server funcionando...');
+    res.write('<h1> Server funcionando... </h1>');
+    res.write('<p>MI sitio asombroso</p>');
+    res.end();
 });
 
 server.listen(3000,'127.0.0.1',function(){
